16d9f64bedc7ece7b701a0c974423f8ab6fa10e8
G384962023 <238496@st.takushoku-u.ac.jp>
2024-06-25 22:51:52 +0900

    課題3

--- kazuate.js
+++ kazuate.js
@@ -17,25 +17,22 @@ hantei();
 function hantei() {
   // 将来ここでは 4 ではなくテキストボックスに指定された数値を yoso に代入する
   let yoso = 4;
+  let n  = Number(yoso);
   
   // 課題3-1: 正解判定する
   // kotae と yoso が一致するかどうか調べて結果を出力
   // 課題3-1における出力先はコンソール
-  for(kaisu = 1; kaisu <= 4; kaisu++){
-    console.log(kaisu + "回目の予想:");
-    if(kaisu < 4 && kaisu >= 1 && yoso === kotae) {
+  if(kaisu >= 4){
+    console.log("答えは "+ kotae + "でした．すでにゲームは終わっています");
+  } else {
+    if(n === kotae){
       console.log("正解です. おめでとう!");
     }
-    else if (kaisu <= 2 && yoso > kotae){
+    else if (n > kotae){
       console.log("まちがい. 答えはもっと小さいですよ");
-      }
-    else if(kaisu<= 2 && yoso < kotae){
+    }
+    else {
       console.log("まちがい. 答えはもっと大きいですよ");
     }
-    else if (kaisu ===3 && yoso===! kotae){
-      console.log("まちがい.残念でした答えは" + kotae +"です.");
-    }
-    else if(kaisu ===4)
-      console.log("答えは "+ kotae + "でした．すでにゲームは終わっています");
   }
 }
