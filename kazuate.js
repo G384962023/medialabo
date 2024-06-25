defb025c86f5b3c4f279ee08578da01a8861d87b
G384962023 <238496@st.takushoku-u.ac.jp>
2024-06-25 23:07:04 +0900

    課題3

--- kazuate.js
+++ kazuate.js
@@ -24,7 +24,7 @@ function hantei() {
   // 課題3-1: 正解判定する
   // kotae と yoso が一致するかどうか調べて結果を出力
   // 課題3-1における出力先はコンソール
-  console.log(kaisu + '回目の予想')
+  console.log(kaisu + '回目の予想' + yoso)
   if(kaisu >= 4){
     console.log("答えは "+ kotae + "でした．すでにゲームは終わっています");
   } else {
