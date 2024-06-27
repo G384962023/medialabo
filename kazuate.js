1b03cfa0c92b70b193774d6aa6eb2955829e4ee3
G384962023 <238496@st.takushoku-u.ac.jp>
2024-06-27 21:15:56 +0900

    課題4

--- kazuate.js
+++ kazuate.js
@@ -9,9 +9,6 @@ let kaisu = 0;
 // 将来以下の hantei(); の4回の呼び出しを全て削除する
 // 代わりにここでは，ボタンを押したら hantei() を呼び出すイベント処理をする
 hantei();
-hantei();
-hantei();
-hantei();
 
 // ボタンを押した後の処理をする関数 hantei() の定義
 function hantei() {
