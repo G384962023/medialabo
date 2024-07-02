0eb3737b654eaf7df615f2add42c988aac96102d
G384962023 <238496@st.takushoku-u.ac.jp>
2024-07-03 00:43:37 +0900

    課題4

--- weather.js
+++ weather.js
@@ -48,5 +48,5 @@ let data = {
 ////////// 課題3-2 ここからプログラムを書こう
 let div = document.querySelector('div#result');
 let p = document.createElement('p');
-p.textContent = (data.coord.lon);
-div.insertAdjacentElement('afterend' , p);
+p.textContent = 'unnko';
+div.insertAdjacentElement('afterend',p);
