7e41b2ff83ee08e49fc1caffe25486db78907a99
G384962023 <238496@st.takushoku-u.ac.jp>
2024-07-04 14:43:15 +0900

    練習5-3

--- practice-event/event-list.js
+++ practice-event/event-list.js
@@ -20,23 +20,23 @@ c5.addEventListener('mouseleave', changeColor);
 
 // cell6: フォーカスすると色が変わる
 let c6 = document.querySelector('#cell6');
-c6.addEventListener('click', changeColor);
+c6.addEventListener('focus', changeColor);
 
 // cell7: フォーカスが外れると色が変わる
 let c7 = document.querySelector('#cell7');
-c7.addEventListener('click', changeColor);
+c7.addEventListener('blur', changeColor);
 
 // cell8: キーボードのキー入力すると色が変わる
 let c8 = document.querySelector('#cell8');
-c8.addEventListener('click', changeColor);
+c8.addEventListener('keypress', changeColor);
 
 // cell9: キーボードのキーを押すと色が変わる
 let c9 = document.querySelector('#cell9');
-c9.addEventListener('click', changeColor);
+c9.addEventListener('keydown', changeColor);
 
 // cell10: キーボードのキーを離すと色が変わる
 let c10 = document.querySelector('#cell10');
-c10.addEventListener('click', changeColor);
+c10.addEventListener('keyup', changeColor);
 
 function changeColor(event) {
 	// ランダムな RGB の色
