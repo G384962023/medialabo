4bc31d7d015ecdce371ba105cf081db5d5f1899b
G384962023 <238496@st.takushoku-u.ac.jp>
2024-07-04 16:38:27 +0900

    課題5-1

--- kazuate.js
+++ kazuate.js
@@ -2,6 +2,7 @@
 let kotae = Math.floor(Math.random()*10) + 1;
 console.log('答え（デバッグ用）: ' + kotae);
 let result;
+
 // 入力回数（予想回数）
 let kaisu = 0;
 
@@ -10,29 +11,26 @@ let kaisu = 0;
 // 代わりにここでは，ボタンを押したら hantei() を呼び出すイベント処理をする
 let a = document.querySelector('button#print');
 a.addEventListener('click', hantei);
+
 // ボタンを押した後の処理をする関数 hantei() の定義
 function hantei() {
   kaisu = kaisu + 1;
-
   // 将来ここでは 4 ではなくテキストボックスに指定された数値を yoso に代入する
   let i = document.querySelector('input[name="seisu"]');
   let shimei = i.value;
   let n  = Number(yoso);
-  
   // 課題3-1: 正解判定する
   // kotae と yoso が一致するかどうか調べて結果を出力
   // 課題3-1における出力先はコンソール
-
+  
   if(kaisu >= 4){
     result = "答えは "+ kotae + "でした．すでにゲームは終わっています";
   } else {
     if(n === kotae){
       result = "正解です. おめでとう!";
-    }
-    else if (n > kotae){
+    } else if (n > kotae){
       result ="まちがい. 答えはもっと小さいですよ";
-    }
-    else {
+    } else {
       result = "まちがい. 答えはもっと大きいですよ";
     }
   }
