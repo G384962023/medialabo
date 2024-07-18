6a87312846e98b2c18b6e32065c170fa919b7b8d
G384962023 <238496@st.takushoku-u.ac.jp>
2024-07-18 14:35:22 +0900

    課題3

--- weather.js
+++ weather.js
@@ -59,6 +59,13 @@ console.log(data.wind.deg);
 console.log(data.name);
 */
 
+//print(data);
+
+function print(data){
+
+}
+
+
 let div = document.querySelector('div#result');
 let h1 = document.createElement('h1');
 h1.textContent = "世界の天気";
@@ -83,25 +90,40 @@ function sendRequest() {
   let s = document.querySelector('#print');
   let idx = s.selectedIndex;
   let os = s.querySelectorAll('option');
+
+  axios.get(url)
+    .then(showResult)   // 通信成功
+    .catch(showError)   // 通信失敗
+    .then(finish);      // 通信の最後の処理
 }
 
-h2.textContent = "検索結果1件";
-div.insertAdjacentElement('beforeend', h2);
-li1.textContent = "緯度:" + data.coord.lon;
-div.insertAdjacentElement('beforeend', li1);
-li2.textContent = "経度:" + data.coord.lat;
-div.insertAdjacentElement('beforeend', li2);
-li3.textContent = "天気:" + data.weather[0].description;
-div.insertAdjacentElement('beforeend', li3);
-li4.textContent = "最低気温:" + data.main.temp_min;
-div.insertAdjacentElement('beforeend', li4);
-li5.textContent = "最高気温:" + data.main.temp_max;
-div.insertAdjacentElement('beforeend', li5);
-li6.textContent = "湿度:" + data.main.humidity;
-div.insertAdjacentElement('beforeend', li6);
-li7.textContent = "風速:" + data.wind.speed;
-div.insertAdjacentElement('beforeend', li7);
-li8.textContent = "風向:" + data.wind.deg;
-div.insertAdjacentElement('beforeend', li8);
-li9.textContent = "都市名:" + data.name;
-div.insertAdjacentElement('beforeend', li9);
+function showResult(resp) {
+  // サーバから送られてきたデータを出力
+  let data = resp.data;
+
+  // data が文字列型なら，オブジェクトに変換する
+  if (typeof data === 'string') {
+      data = JSON.parse(data);
+  }
+
+  h2.textContent = "検索結果1件";
+  div.insertAdjacentElement('beforeend', h2);
+  li1.textContent = "緯度:" + data.coord.lon;
+  div.insertAdjacentElement('beforeend', li1);
+  li2.textContent = "経度:" + data.coord.lat;
+  div.insertAdjacentElement('beforeend', li2);
+  li3.textContent = "天気:" + data.weather[0].description;
+  div.insertAdjacentElement('beforeend', li3);
+  li4.textContent = "最低気温:" + data.main.temp_min;
+  div.insertAdjacentElement('beforeend', li4);
+  li5.textContent = "最高気温:" + data.main.temp_max;
+  div.insertAdjacentElement('beforeend', li5);
+  li6.textContent = "湿度:" + data.main.humidity;
+  div.insertAdjacentElement('beforeend', li6);
+  li7.textContent = "風速:" + data.wind.speed;
+  div.insertAdjacentElement('beforeend', li7);
+  li8.textContent = "風向:" + data.wind.deg;
+  div.insertAdjacentElement('beforeend', li8);
+  li9.textContent = "都市名:" + data.name;
+  div.insertAdjacentElement('beforeend', li9);
+}
